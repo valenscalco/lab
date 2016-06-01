60d247fa7161afefa9a218657772b9f995deb197
juanbrz92 <juanbrz92@hotmail.com>
2016-05-31 21:56:01 -0300

    tp1brizuela

---
+++ alumnos/55007/tp1brizuela/HACHA/cortar.h
@@ -0,0 +1,15 @@
+#ifndef CORTAR_H
+#define CORTAR_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+
+
+
+
+#endif
